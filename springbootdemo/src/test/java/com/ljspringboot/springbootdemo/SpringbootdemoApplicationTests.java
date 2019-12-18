f442fa2cc522d958180e39d4da97ca4ca21593a3
lijian <1129820294@qq.com>
2019-12-18 09:43:26 +0800

    SpringBoot快速入门
    SpringBoot快速入门

---
+++ springbootdemo/src/test/java/com/ljspringboot/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ljspringboot.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
